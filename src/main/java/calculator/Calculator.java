0b7f722453cfbbcaab98523abb7ed9a161a5376b
Juan Luis Márquez <juan.luis.marquezcan@gmail.com>
2026-03-11 12:16:14 +0100

    feat: tests endeds.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,4 +5,20 @@ public class Calculator{
     public int Sum(int number1, int number2){
         return number1+number2;
     }
+    public int Rest(int number1, int number2){
+        return  number1-number2;
+    }
+
+    public int Product(int number1, int number2){
+        return number1*number2;
+    }
+
+    public int IntegerDivision(int number1, int number2){
+        if(number1%number2!=0) System.out.println("La division no es exacta, y su resto es "+ number1%number2);
+        return (int)(number1/number2);
+    }
+
+    public double DoubleDivision(double number1, double number2){
+
+    }
 }
--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -11,19 +11,20 @@ public class calculatorTest {
     public void integerRestTest(){
         assertEquals(1, calculator.Rest(4,1));
         assertEquals(-1, calculator.Rest(2, 3));
-        assertThrows(Exception.class, ()->calculator.Rest(1, "2"));
     }
     public void integerProductTest(){
         assertEquals(4, calculator.Product(2,2));
         assertEquals(-4, calculator.Product(2,-2));
     }
     public void integerDivisionTest(){
-        assertEquals(1, calculator.integerDivision(2,2));
-        assertEquals(2, calculator.integerDivision(4,2));
-        assertEquals(2, calculator.integerDivision(5, 2));
+        assertEquals(1, calculator.IntegerDivision(2,2));
+        assertEquals(2, calculator.IntegerDivision(4,2));
+        assertEquals(2, calculator.IntegerDivision(5, 2));
+        assertThrows(ArithmeticException.class, () -> calculator.IntegerDivision(5, 0));
     }
     public void doubleDivisionTest(){
-        assertEquals(1, calculator.doubleDivision(2.0, 2.0));
-        assertEquals(2.5, calculator.doubleDivision(5, 2.0));
+        assertEquals(1, calculator.DoubleDivision(2.0, 2.0));
+        assertEquals(2.5, calculator.DoubleDivision(5, 2.0));
+        assertThrows(ArithmeticException.class, () -> calculator.DoubleDivision(5, 0));
     }
 }
