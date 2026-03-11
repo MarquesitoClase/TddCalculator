149643e311f8e72d7701b8402b8aab9dd4e8a4bf
Juan Luis Márquez <juan.luis.marquezcan@gmail.com>
2026-03-11 11:59:12 +0100

    feat: ended all calculatorTests, and make Sum method into Calculator.java

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -1,4 +1,4 @@
-import org.calculator.Calculator;
+import calculator.Calculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -12,5 +12,18 @@ public class calculatorTest {
         assertEquals(1, calculator.Rest(4,1));
         assertEquals(-1, calculator.Rest(2, 3));
         assertThrows(Exception.class, ()->calculator.Rest(1, "2"));
+    }
+    public void integerProductTest(){
+        assertEquals(4, calculator.Product(2,2));
+        assertEquals(-4, calculator.Product(2,-2));
+    }
+    public void integerDivisionTest(){
+        assertEquals(1, calculator.integerDivision(2,2));
+        assertEquals(2, calculator.integerDivision(4,2));
+        assertEquals(2, calculator.integerDivision(5, 2));
+    }
+    public void doubleDivisionTest(){
+        assertEquals(1, calculator.doubleDivision(2.0, 2.0));
+        assertEquals(2.5, calculator.doubleDivision(5, 2.0));
     }
 }
