8bb6c16530bc3801008c8321d79e4558c6ae4920
Juan Luis Márquez <juan.luis.marquezcan@gmail.com>
2026-03-11 12:47:16 +0100

    rename test class, to do CapittalLeter the first one

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class calculatorTest {
+public class CalculatorTest {
     Calculator calculator = new Calculator();
     @Test
     public void IntegerSumTest(){
