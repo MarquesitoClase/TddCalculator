e321930630a610b4a07e08f23562f5e431a6ab51
Juan Luis Márquez <juan.luis.marquezcan@gmail.com>
2026-03-11 11:46:10 +0100

    fix: changed assertThrows to one arrowEay to do it that I found on internet. Not know what is better. Looks is more correct the -> one nowadays.

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -1,10 +1,16 @@
 import org.calculator.Calculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class calculatorTest {
     Calculator calculator = new Calculator();
     public void IntegerSumTest(){
         assertEquals(4, calculator.Sum(1, 3));
+    }
+    public void integerRestTest(){
+        assertEquals(1, calculator.Rest(4,1));
+        assertEquals(-1, calculator.Rest(2, 3));
+        assertThrows(Exception.class, ()->calculator.Rest(1, "2"));
     }
 }
